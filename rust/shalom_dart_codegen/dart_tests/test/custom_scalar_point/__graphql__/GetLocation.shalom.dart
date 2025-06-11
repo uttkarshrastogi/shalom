7b70d4a14f845037b733496df7151d460e30c9c2
uttkarshrastogi <50842869+uttkarshrastogi@users.noreply.github.com>
2025-06-11 19:23:41 +0530

    fixes done--

---
+++ rust/shalom_dart_codegen/dart_tests/test/custom_scalar_point/__graphql__/GetLocation.shalom.dart
@@ -0,0 +1,137 @@
+import 'package:shalom_core/shalom_core.dart';
+
+typedef JsonObject = Map<String, dynamic>;
+// ignore_for_file: non_constant_identifier_names
+// ignore_for_file: camel_case_types
+
+class GetLocationResponse {
+  /// class members
+
+  final GetLocation_getLocation? getLocation;
+
+  // keywordargs constructor
+  GetLocationResponse({this.getLocation});
+  static GetLocationResponse fromJson(JsonObject data) {
+    final GetLocation_getLocation? getLocation_value;
+
+    final JsonObject? getLocation$raw = data['getLocation'];
+    if (getLocation$raw != null) {
+      getLocation_value = GetLocation_getLocation.fromJson(getLocation$raw);
+    } else {
+      getLocation_value = null;
+    }
+
+    return GetLocationResponse(getLocation: getLocation_value);
+  }
+
+  GetLocationResponse updateWithJson(JsonObject data) {
+    final GetLocation_getLocation? getLocation_value;
+    if (data.containsKey('getLocation')) {
+      final JsonObject? getLocation$raw = data['getLocation'];
+      if (getLocation$raw != null) {
+        getLocation_value = GetLocation_getLocation.fromJson(getLocation$raw);
+      } else {
+        getLocation_value = null;
+      }
+    } else {
+      getLocation_value = getLocation;
+    }
+
+    return GetLocationResponse(getLocation: getLocation_value);
+  }
+
+  @override
+  bool operator ==(Object other) {
+    return identical(this, other) ||
+        (other is GetLocationResponse && other.getLocation == getLocation);
+  }
+
+  @override
+  int get hashCode => getLocation.hashCode;
+
+  JsonObject toJson() {
+    return {'getLocation': getLocation?.toJson()};
+  }
+}
+
+// ------------ OBJECT DEFINITIONS -------------
+
+class GetLocation_getLocation {
+  /// class members
+
+  final String id;
+
+  final Point? coords;
+
+  // keywordargs constructor
+  GetLocation_getLocation({required this.id, this.coords});
+  static GetLocation_getLocation fromJson(JsonObject data) {
+    final String id_value;
+
+    id_value = data['id'];
+
+    final Point? coords_value;
+
+    coords_value =
+        data['coords'] == null
+            ? null
+            : customScalars['Point']!.deserialize(data['coords']);
+
+    return GetLocation_getLocation(id: id_value, coords: coords_value);
+  }
+
+  GetLocation_getLocation updateWithJson(JsonObject data) {
+    final String id_value;
+    if (data.containsKey('id')) {
+      id_value = data['id'];
+    } else {
+      id_value = id;
+    }
+
+    final Point? coords_value;
+    if (data.containsKey('coords')) {
+      coords_value = data['coords'];
+    } else {
+      coords_value = coords;
+    }
+
+    return GetLocation_getLocation(id: id_value, coords: coords_value);
+  }
+
+  @override
+  bool operator ==(Object other) {
+    return identical(this, other) ||
+        (other is GetLocation_getLocation &&
+            other.id == id &&
+            other.coords == coords);
+  }
+
+  @override
+  int get hashCode => Object.hashAll([id, coords]);
+
+  JsonObject toJson() {
+    return {'id': id, 'coords': coords};
+  }
+}
+
+// ------------ END OBJECT DEFINITIONS -------------
+
+class RequestGetLocation extends Requestable {
+  RequestGetLocation();
+
+  @override
+  Request toRequest() {
+    JsonObject variablesJson = {};
+    return Request(
+      query: r"""query GetLocation {
+  getLocation {
+    id
+    coords
+  }
+}""",
+      variables: variablesJson,
+      opType: OperationType.Query,
+      StringopName: 'GetLocation',
+    );
+  }
+}
---
+++ rust/shalom_dart_codegen/dart_tests/test/custom_scalar_point/__graphql__/schema.shalom.dart
@@ -0,0 +1,8 @@
+// ignore_for_file: constant_identifier_names
+
+// ------------ Enum DEFINITIONS -------------
+
+// ------------ END Enum DEFINITIONS -------------
+// ------------ Input DEFINITIONS -------------
+
+// ------------ END Input DEFINITIONS -------------
