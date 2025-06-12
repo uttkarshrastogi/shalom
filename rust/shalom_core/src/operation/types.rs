054c273320e187b27d9a2e5eed2c1e1a1a31be64
uttkarshrastogi <50842869+uttkarshrastogi@users.noreply.github.com>
2025-06-12 20:11:29 +0530

    changes done

--- rust/shalom_core/src/operation/types.rs
+++ rust/shalom_core/src/operation/types.rs
@@ -113,8 +113,6 @@ impl EnumSelection {
 
 pub fn dart_type_for_scalar(scalar_name: &str, ctx: &SharedShalomGlobalContext) -> String {
     if let Some(mapping) = ctx.find_custom_scalar(scalar_name) {
-        // Example: "lib/src/point.dart#Point"
-        // Just extract the type name after '#'
         return mapping
             .scalar_dart_type
             .split('#')
